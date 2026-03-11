621485b6ab9b291c4ef26592071d2f03c4874646
girish123946 <gggirishgokul@gmail.com>
2026-03-11 16:25:37 +0530

    Add files via upload

---
+++ Settings.jsx
@@ -0,0 +1,10 @@
+function Settings() {
+  return (
+    <div className="page">
+      <h1>Settings</h1>
+      <p>Manage application settings here.</p>
+    </div>
+  );
+}
+
+export default Settings;
